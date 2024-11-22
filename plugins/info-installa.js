351c6e23f88eb0d1765feb1fc5a7464834ff7a1e
Gabsbot1 <numerofoxi@gmail.com>
2024-11-22 19:00:41 +0100

    info-installa.js

--- plugins/info-installa.js
+++ plugins/info-installa.js
@@ -20,35 +20,30 @@ let neww = performance.now()
 let speed = (neww - old).toFixed(4)
 let prova = { "key": {"participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo"
 }, "message": { 
-"orderMessage": { text: '𝐁𝐢𝐱𝐛𝐲 𝐕𝐢𝐬𝐢𝐨𝐧 🔮',
+"orderMessage": { text: 'Gabs Bot 🔮',
 "itemCount": 2023,
 "status": 1,
 "surface" : 1,
-    "message": '𝐒𝐜𝐚𝐫𝐢𝐜𝐚 𝐁𝐢𝐱𝐛𝐲 𝐕𝐢𝐬𝐢𝐨𝐧 🔮',
+    "message": '𝐒𝐜𝐚𝐫𝐢𝐜𝐚 Gabs Bot  🔮',
 "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=15395490858:+1 (539) 549-0858\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD`
 }}, "participant": "0@s.whatsapp.net"
 }
 let info = `
 ══════ •⊰✦⊱• ══════
 𝐂𝐨𝐦𝐚𝐧𝐝𝐢 𝐩𝐞𝐫 𝐢𝐧𝐬𝐭𝐚𝐥𝐥𝐚𝐫𝐞
-✧ cd
-✧ termux-setup-storage
-✧ apt update 
-✧ pkg upgrade 
-✧ pkg install git -y
-✧ pkg install nodejs -y
-✧ pkg install ffmpeg -y
-✧ pkg install imagemagick -y
-✧ pkg install yarn
-✧ git clone https://github.com/MoonContentCreator/BixbyBot-Md.git
-✧ cd BixbyBot-Md 
-✧ yarn install 
-✧ npm install
-✧ npm update
-✧ npm start
+cd /sdcard/Gabs-Md
+cd /sdcard && rm -rf BixbyBot && git clone https://github.com/Gabsbot1/Gabs-Md.git
+termux-setup-storage
+pkg upgrade -y && pkg update -y
+pkg install git -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkg install yarn
+yarn
+npm start
 
 𝐒𝐜𝐚𝐫𝐢𝐜𝐚𝐫𝐞 𝐥𝐚 𝐜𝐚𝐫𝐭𝐞𝐥𝐥𝐚 𝐝𝐞𝐥 𝐛𝐨𝐭 📱
-✧ cd /sdcard && rm -rf BixbyBot-Md && git clone https://github.com/MoonContentCreator/BixbyBot-Md.git && cd BixbyBot-Md && npm start
+✧ cd /sdcard && rm -rf BixbyBot && git clone https://github.com/Gabsbot1/Gabs-Md.git
 ══════ •⊰✦⊱• ══════
 `.trim() 
 conn.reply(m.chat, info,prova, m, {
